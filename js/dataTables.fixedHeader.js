fdf63ac39b1278697953cf03e5ca78278afdb671
Allan Jardine <allan.jardine@sprymedia.co.uk>
2022-11-04 11:30:45 +0000

    859c87197a3661ae36209bb1cadf6513a9d0d0df
    Sync to source repo @859c87197a3661ae36209bb1cadf6513a9d0d0df

--- js/dataTables.fixedHeader.js
+++ js/dataTables.fixedHeader.js
@@ -1,47 +1,6 @@
-
 /*! FixedHeader 3.2.4
  * ©2009-2022 SpryMedia Ltd - datatables.net/license
  */
-
-(function( factory ){
-	if ( typeof define === 'function' && define.amd ) {
-		// AMD
-		define( ['jquery', 'datatables.net'], function ( $ ) {
-			return factory( $, window, document );
-		} );
-	}
-	else if ( typeof exports === 'object' ) {
-		// CommonJS
-		module.exports = function (root, $) {
-			if ( ! root ) {
-				// CommonJS environments without a window global must pass a
-				// root. This will give an error otherwise
-				root = window;
-			}
-
-			if ( ! $ ) {
-				$ = typeof window !== 'undefined' ? // jQuery's factory checks for a global window
-					require('jquery') :
-					require('jquery')( root );
-			}
-
-			if ( ! $.fn.dataTable ) {
-				require('datatables.net')(root, $);
-			}
-
-
-			return factory( $, root, root.document );
-		};
-	}
-	else {
-		// Browser
-		factory( jQuery, window, document );
-	}
-}(function( $, window, document, undefined ) {
-'use strict';
-var DataTable = $.fn.dataTable;
-
-
 
 /**
  * @summary     FixedHeader
@@ -1091,7 +1050,3 @@ $.each( ['header', 'footer'], function ( i, el ) {
 		} );
 	} );
 } );
-
-
-return DataTable;
-}));
