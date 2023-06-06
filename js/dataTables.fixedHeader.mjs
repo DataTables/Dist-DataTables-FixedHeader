ea38ad739349427d8d77970ebb00c4702fd85643
Allan Jardine <allan.jardine@sprymedia.co.uk>
2023-06-06 13:10:30 +0100

    3f717a0badf83c94e5d465a6ee8000c50042eb7a New: Dark mode support for DataTables and Bootstrap 5 styling
    Sync to source repo @3f717a0badf83c94e5d465a6ee8000c50042eb7a

--- js/dataTables.fixedHeader.mjs
+++ js/dataTables.fixedHeader.mjs
@@ -2,8 +2,11 @@
  * © SpryMedia Ltd - datatables.net/license
  */
 
-import $ from 'jquery';
+import jQuery from 'jquery';
 import DataTable from 'datatables.net';
+
+// Allow reassignment of the $ variable
+let $ = jQuery;
 
 
 /**
