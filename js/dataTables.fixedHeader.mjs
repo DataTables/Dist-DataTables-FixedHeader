b038a3c81eced61de73e6e1e71bdd5504051d959
dtbuild <dtbuild@rigil>
2026-09-22 10:37:34 +0000

    aac6e2480ee3a88a76b71a1f8be940ae9f53526b Release 5.1.0
    Sync to source repo @aac6e2480ee3a88a76b71a1f8be940ae9f53526b

--- js/dataTables.fixedHeader.mjs
+++ js/dataTables.fixedHeader.mjs
@@ -1,4 +1,4 @@
-/*! FixedHeader 5.0.0 for DataTables
+/*! FixedHeader 5.1.0 for DataTables
  * Copyright (c) SpryMedia Ltd - datatables.net/license
  */
 
@@ -888,7 +888,7 @@ FixedHeader.defaults = {
     footerOffset: 0
 };
 /** Version */
-FixedHeader.version = '5.0.0';
+FixedHeader.version = '5.1.0';
 
 
 if (!DataTable || !DataTable.versionCheck('3.1')) {
