0d14f027e0973182d68190c5eeddfc73fa2e1265
dtbuild <dtbuild@rigil>
2026-09-24 17:10:13 +0000

    879ef81837bcfd0809e456c6e3f4770b40b94326 Fix: When there was a nested table in the host DataTable, the headers would clash, resulting in rendering errors
    https://datatables.net/forums/discussion/81838/
    
    The issue was the scoping of finding elements. I didn't limit it to just
    the children of the host table when looking for a target element. Same
    with cloning the colgroup.
    
    Sync to source repo @879ef81837bcfd0809e456c6e3f4770b40b94326

--- js/dataTables.fixedHeader.mjs
+++ js/dataTables.fixedHeader.mjs
@@ -342,7 +342,7 @@ class FixedHeader {
             // floating element and clone into the original table. Note that the
             // order is important in Chrome. It must be colgroup, thead, tbody,
             // tfoot. Otherwise a "jitter" when scrolling will occur.
-            itemDom.placeholder.insertAfter(itemDom.host.find(item === 'header' ? 'colgroup' : 'tbody'));
+            itemDom.placeholder.insertAfter(itemDom.host.children(item === 'header' ? 'colgroup' : 'tbody'));
             itemDom.floating.append(itemElement);
             this._widths(itemDom);
             return scrollLeftUpdate;
@@ -454,7 +454,7 @@ class FixedHeader {
             if (itemDom.host) {
                 if (!itemDom.host.contains(tablePart)) {
                     if (item === 'header') {
-                        tablePart.insertAfter(itemDom.host.find('colgroup'));
+                        tablePart.insertAfter(itemDom.host.children('colgroup'));
                     }
                     else {
                         itemDom.host.append(tablePart);
@@ -861,7 +861,7 @@ class FixedHeader {
         // header
         var cols = itemDom.placeholder
             .parent()
-            .find('colgroup')
+            .children('colgroup')
             .clone(true)
             .appendTo(itemDom.floating)
             .find('col');
